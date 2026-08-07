31feb6f276e0381bd7755b918d1e0a42d3633e3c
Keamogetswe Monageng <koketsokea1@gmail.com>
2026-08-08 01:21:49 +0200

    Modified Code
    Allow it to ask how many dice to roll

--- dice_rolling_game.py
+++ dice_rolling_game.py
@@ -1,15 +1,16 @@
-import random    #module that allows us to generate a random number
+import random   #module that allows us to generate a random number
 
-while True:    #better than declaring a variable and setting it to true
-    #Ask user if they want to roll the dice
-    choice = input('Roll the dice? (y/n): ').lower()  #convert the answer into lowercase so we don't have too many conditions in the if statement
+while True:   #better than declaring a variable and setting it to true
+    choice = input('Start the game? (y/n): ').lower()   #convert the answer into lowercase so we don't have too many conditions in the if statement
 
     if choice == 'y':
-        die1 = random.randint(1, 6)   #method to generate a random number between 1 & 6
-        die2 = random.randint(1, 6)
-        print(f'({die1}, {die2})')    #use f string for simplicity
-    elif choice == 'n':
+        num_dice = int(input('How many dice would you like to roll? '))
+
+        for i in range(num_dice):
+            dice = random.randint(1, 6)    #method to generate a random number between 1 & 6
+            print(dice)
+    elif choice =='n':
         print('Thanks for playing!!!')
         break
     else:
-        print('Invalid choice!!!')
+        print('Invalid Input!!!')
