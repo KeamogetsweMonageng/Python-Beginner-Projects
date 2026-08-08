1bea27274cd81ca1b1e744733eaf688ccb9c1c1b
Keamogetswe Monageng <koketsokea1@gmail.com>
2026-08-08 02:26:52 +0200

    Modify number_guessing_game.py
    Specify range, Make it so you only have a certain amount of guesses and keep track of number of attempts

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,16 +1,24 @@
 import random
 
-number_to_guess = random.randint(1, 100)   #generate a random number between 1 & 100. Outside loop to do it once
+minimum_range = int(input('Specify your minimum range: '))
+maximum_range = int(input('Specify your maximum range: '))
+number_to_guess = random.randint(minimum_range, maximum_range)   #generate a random number between the specified ranges. 
+guesses = 5   #To run the code only 5 times
+attempts = 0   #To keep track of the number of guesses already made
 
-while True:
-    try:   #Error handling so the program does not crash
-        guessed_number = int(input('Guess the number between 1 & 100: '))
+for i in range(guesses):
+    try:   #Error handling
+        guessed_number = int(input(f"Guess the number between {minimum_range} and {maximum_range}, You have {5 - i} guesses left:  "))
+        attempts += 1
         if guessed_number > number_to_guess:
-                print('Too high!')
+            print('Too high!')
         elif guessed_number < number_to_guess:
-                print('Too low!')
+            print('Too low!')
         elif guessed_number == number_to_guess:
-                print('congrats! you guessed the number!')
-                break
+            print('congrats! you guessed the number!')
+            print(f"Number of attempts: {attempts}")
+            break
     except ValueError:
         print('Please enter a valid number')
+else:
+    print('You lost! Ran out of guesses')
